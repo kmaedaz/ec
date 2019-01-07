277bce0eabbfbb1b6de96bc509b58e1e6823a57f
root <root@ik1-304-12240.vs.sakura.ne.jp>
2019-01-08 06:42:35 +0900

    regist questionnaire

---
+++ src/Eccube/Form/Type/Admin/QuestionnaireType.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ * This file is Customized File
+ */
+
+
+namespace Eccube\Form\Type\Admin;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Eccube\Application;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class QuestionnaireType.
+ */
+class QuestionnaireType extends AbstractType
+{
+    /**
+     * @var Application
+     */
+    public $app;
+
+    /**
+     * QuestionnaireType constructor.
+     *
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        /**
+         * @var ArrayCollection $arrCategory array of category
+         */
+        $arrCategory = $this->app['eccube.repository.category']->getGeneralList(null, true);
+
+        $builder
+            // 基本情報
+            ->add('name', 'text', array(
+                'label' => 'アンケート名',
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                ),
+            ))
+            // 募集期間(From)
+            ->add('application_period_from', 'text', array(
+                'label' => '募集期間(From)',
+                'constraints' => array(
+                    new Assert\Regex(array('pattern' => '/^[0-9]{4}\/([0][1-9]|[1][0-2])\/([0][1-9]|[1-2][0-9]|[3][0-1]) ([0-1][0-9]|[2][0-3]):[0-5][0-9]$/')),
+                ),
+            ))
+            // 募集期間(To)
+            ->add('application_period_to', 'text', array(
+                'label' => '募集期間(To)',
+                'constraints' => array(
+                    new Assert\Regex(array('pattern' => '/^[0-9]{4}\/([0][1-9]|[1][0-2])\/([0][1-9]|[1-2][0-9]|[3][0-1]) ([0-1][0-9]|[2][0-3]):[0-5][0-9]$/')),
+                ),
+            ))
+            ->add('attachment_file', 'file', array(
+                'label' => '添付ファイル',
+                'multiple' => true,
+                'required' => false,
+                'mapped' => false,
+            ))
+            ->add('target', 'customer_type', array(
+                'required' => true,
+                'label' => '公開対象',
+                'multiple' => false,
+                'expanded' => false,
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                ),
+            ))
+            // 右ブロック
+            ->add('Status', 'disp', array(
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                ),
+            ))
+            // 添付
+            ->add('attachments', 'collection', array(
+                'type' => 'hidden',
+                'prototype' => true,
+                'mapped' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+            ))
+            ->add('add_attachments', 'collection', array(
+                'type' => 'hidden',
+                'prototype' => true,
+                'mapped' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+            ))
+            ->add('delete_attachments', 'collection', array(
+                'type' => 'hidden',
+                'prototype' => true,
+                'mapped' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'admin_questionnaire';
+    }
+}
---
+++ src/Eccube/Form/Type/Admin/SearchQuestionnaireType.php
@@ -0,0 +1,105 @@
+<?php
+/*
+ * This file is Custmized File.
+ */
+
+
+namespace Eccube\Form\Type\Admin;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SearchQuestionnaireType extends AbstractType
+{
+    public $app;
+
+    public function __construct(\Silex\Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $app = $this->app;
+
+        $builder
+            ->add('id', 'text', array(
+                'label' => 'アンケートID',
+                'required' => false,
+            ))
+            ->add('name', 'text', array(
+                'label' => 'アンケート名',
+                'required' => false,
+            ))
+            ->add('status', 'disp', array(
+                'label' => '種別',
+                'multiple'=> true,
+                'required' => false,
+            ))
+            ->add('application_period_from', 'date', array(
+                'label' => '公開期間(From)',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'empty_value' => array('year' => '----', 'month' => '--', 'day' => '--'),
+            ))
+            ->add('application_period_to', 'date', array(
+                'label' => '公開期間(To)',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'empty_value' => array('year' => '----', 'month' => '--', 'day' => '--'),
+            ))
+            ->add('create_date_start', 'date', array(
+                'label' => '登録日(FROM)',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'empty_value' => array('year' => '----', 'month' => '--', 'day' => '--'),
+            ))
+            ->add('create_date_end', 'date', array(
+                'label' => '登録日(TO)',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'empty_value' => array('year' => '----', 'month' => '--', 'day' => '--'),
+            ))
+            ->add('update_date_start', 'date', array(
+                'label' => '更新日(FROM)',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'empty_value' => array('year' => '----', 'month' => '--', 'day' => '--'),
+            ))
+            ->add('update_date_end', 'date', array(
+                'label' => '更新日(TO)',
+                'required' => false,
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'empty_value' => array('year' => '----', 'month' => '--', 'day' => '--'),
+            ))
+            ->add('link_status', 'hidden', array(
+                'mapped' => false,
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'admin_search_questionnaire';
+    }
+}
